253417572171d37804de841009ca9c2c928b137a
lukka12 <47463180+lukka12@users.noreply.github.com>
2021-11-07 13:57:54 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -11,7 +11,7 @@ import aiofiles
 import aiohttp
 import requests
 import wget
-import yt_dlp
+import youtube-dl
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
